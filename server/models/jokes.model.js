3b9c503d91e357e2be0626bf4b3d96e530b48244
acav310 <acavanaugh2013@gmail.com>
2022-07-21 19:41:37 -0400

    validations

--- server/models/jokes.model.js
+++ server/models/jokes.model.js
@@ -3,9 +3,16 @@ const mongoose = require('mongoose');
 
 
 const JokeSchema = new mongoose.Schema({
-    setup: String,
-    punchline: String
-})
+        setup: {
+            type: String,
+            minlength: [10, "setup must be atleast 10 characters"]
+        },
+        punchline: {
+            type: String,
+            minlength: [2, "punchline must be atleast 2 characters"]
+        }
+    },
+);
 
 const Joke = mongoose.model("Joke", JokeSchema)
 
